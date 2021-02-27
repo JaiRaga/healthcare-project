e8caea488ce8c0ce0ff612fa54bedd7ee9277edc
JaiRaga <raga.j.santhosh@gmail.com>
2021-02-27 16:08:54 +0530

    added drawer component and breakpoints where components will be displayed

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import { Fragment } from 'react'
+import React, { Fragment } from 'react'
+import { Hidden } from '@material-ui/core'
 import Navbar from '../layout/Navbar'
+import Drawer from '../layout/Drawer'
 
 const Landing = () => {
 	return (
 		<Fragment>
-			<Navbar />
+			<Hidden only={['xs', 'sm']}>
+				<Navbar />
+			</Hidden>
+			<Hidden only={['md', 'lg', 'xl']}>
+				<Drawer />
+			</Hidden>
+
 			<div>Landing page.</div>
 		</Fragment>
 	)
